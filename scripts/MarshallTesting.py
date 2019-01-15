545e12c7931c2e33ba8288430594fef418559d70
MarshallHunt <marshall.hunt@outlook.com>
2019-01-15 16:28:29 -0600

    work on historical data file

--- scripts/MarshallTesting.py
+++ scripts/MarshallTesting.py
@@ -3,19 +3,32 @@ from hunt import paper_key_id, paper_secret_key
 import pandas as pd
 from logger import logging
 import time
+import HistoricalData
 
 api = tradeapi.REST(paper_key_id, paper_secret_key, 'https://paper-api.alpaca.markets')
-symbol = 'AAPL'
-max_shares = 100
+
+#overall trading strategy
+#BUY CONDITION:
+#               - 3 day moving avg above 10 day moving average
+#               - 3 day moving avg slop positive
+#
+#SELL CONDITION
+#               - 3 day moving avg slope negative
+#                           OR
+#               - 3 day moving avg drops below 10 day
+#                           OR
+#               - Lose of more than 5%
+#PRIORITY CONDITION
+#               - Sort by 200 day moving avg slope
 
 def main():
-    api.submit_order(
-        symbol=symbol,
-        qty=max_shares,
-        side='buy',
-        type='market',
-        time_in_force='day'
-        )
+    df = pd.read_csv('sp500_stocks.csv.csv')
+    df = df.sort_values(by=['Symbol'])
+    stock_list = df['Symbol']
+
+    HistoricalData.last_200_days(stock_list)
+
+    return
 
 if __name__ == '__main__':
     main()

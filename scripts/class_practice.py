89718fcbf05a365ead84ab25b32dab537b06a106
Cameron Carver <35936027+clcarver1130@users.noreply.github.com>
2019-01-15 14:57:09 -0600

    Added secret_key directory to gitignore file and created a class_practice file to tinker around in

---
+++ scripts/class_practice.py
@@ -0,0 +1,20 @@
+
+class Strategy:
+
+    # Create a strategy object - give it a name
+    def __init__(self, name):
+        self.name = name  
+
+
+    # Backtest strategy:
+    def backtest(self, start_time, end_time):
+        '''
+        A backtesting method that allows you to quickly backtest a strategy in a user-passed time frame.
+
+        Inputs:
+        1. Start time in the format: 'YYYY-MM-DD'
+        2. End time in the format: 'YYYY-MM-DD'
+
+        Returns:
+        A strategy report that includes a CSV of trades and prices during the testing period.
+        '''

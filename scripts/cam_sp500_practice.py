7e7510bc953998afa4e54193d2aa3b7a9f3d4f93
Cameron Carver <35936027+clcarver1130@users.noreply.github.com>
2019-01-23 12:07:23 -0600

    Debugged calculate_metrics

--- scripts/cam_sp500_practice.py
+++ scripts/cam_sp500_practice.py
@@ -15,5 +15,28 @@ def main.py():
 
 
 def calculate_metrics(symbols):
+    metric_dict = dict()
     for sym in symbols:
-        hist_close = pull_hist_data(api, sym, '200 days')[['close']] 
+        hist_close = pull_hist_data(api, sym, '200 days')[['close']]
+        metric_dict[sym] = {}
+
+        # 100 Day Metrics:
+        metric_dict[sym]['100_ewma'] =  hist_close[-100:].ewm(span=100).mean().iloc[-1][0]
+        metric_dict[sym]['100_ewma_shited'] = hist_close[-101:-1].ewm(span=100).mean().iloc[-1][0]
+        metric_dict[sym]['100_slope'] = calculate_slope(metric_dict[sym]['100_ewma_shited'], metric_dict[sym]['100_ewma'])
+
+        # 10 Day Metrics:
+        metric_dict[sym]['10_ewma'] =  hist_close[-10:].ewm(span=10).mean().iloc[-1][0]
+        metric_dict[sym]['10_ewma_shited'] = hist_close[-11:-1].ewm(span=10).mean().iloc[-1][0]
+        metric_dict[sym]['10_slope'] = calculate_slope(metric_dict[sym]['10_ewma_shited'], metric_dict[sym]['10_ewma'])
+
+        # 3 Day Metrics:
+        metric_dict[sym]['3_ewma'] =  hist_close[-3:].ewm(span=3).mean().iloc[-1][0]
+        metric_dict[sym]['3_ewma_shited'] = hist_close[-4:-1].ewm(span=3).mean().iloc[-1][0]
+        metric_dict[sym]['3_slope'] = calculate_slope(metric_dict[sym]['3_ewma_shited'], metric_dict[sym]['3_ewma'])
+
+        # Other metrics:
+        metric_dict[sym]['current_price'] = hist_close.iloc[-1]
+
+    # Convert to dict to df:
+    return pd.DataFrame.from_dict(metric_dict, orient='index')

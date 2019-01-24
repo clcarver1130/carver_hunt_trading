3d62553a17db19c90ef2f77ca1237b0a9a6e6076
MarshallHunt <marshall.hunt@outlook.com>
2019-01-24 11:28:08 -0600

    Add files via upload

--- MarshallScripts/MarshallTesting.py
+++ MarshallScripts/MarshallTesting.py
@@ -75,7 +75,7 @@ def first_of_day_trades(df):
     stock_list_updated = HelperFunctions.doIBuy(stock_list_with_positions)
     logging.info('after do i buy')
     #if positions need sold, sell them
-    print(type(stock_list_updated['Sell']))
+    print(type(stock_list_updated.Sell))
     print(type('Yes'))
     tester = stock_list_updated[stock_list_updated.Sell == 'Yes']
     print(tester)

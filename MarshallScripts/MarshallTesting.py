3c5010bdcca97c33dd80856b5338a23e9bb75294
MarshallHunt <marshall.hunt@outlook.com>
2019-01-24 11:09:21 -0600

    Add files via upload

--- MarshallScripts/MarshallTesting.py
+++ MarshallScripts/MarshallTesting.py
@@ -77,7 +77,7 @@ def first_of_day_trades(df):
     #if positions need sold, sell them
     print(type(stock_list_updated['Sell']))
     print(type('Yes'))
-    to_sell = stock_list_updated[stock_list_updated['Sell'] == 'Yes'].index.tolist()
+    to_sell = stock_list_updated[stock_list_updated['Sell'].equals('Yes')].index.tolist()
     for sym in to_sell:
         make_order(api, 'sell', sym, positions[0][sym]['qty'])
     logging.info('after sell orders')

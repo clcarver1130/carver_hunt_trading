0a4f3f1e2cf11f1b80771606d16fdd7405cf67da
Cameron Carver <35936027+clcarver1130@users.noreply.github.com>
2019-01-25 09:50:31 -0600

    Moved back to only market orders

--- scripts/helper_functions.py
+++ scripts/helper_functions.py
@@ -80,15 +80,15 @@ def make_order(api, status, sym, qty, order_type='market', off_set=0.005):
     qty: int, the # of shares to buy or sell
     type: str, the type of order. Market, limit, or stop order. If limit or stop must specify the limit_price or stop_price
     '''
-    if status == 'sell':
-        positions = {p.symbol: p for p in api.list_positions()}
-        current_price = positions[sym].current_price
-        stop_price = float(current_price) * (1-off_set)
-    elif status == 'buy':
-        current_price = pull_hist_data(api, sym, pd.Timestamp.now(), agg='minute', days='1 days')['close'][-1]
-        limit_price = float(current_price) * (1+off_set)
-    else:
-        raise ValueError('Not a valid status. Must be buy or sell')
+    # if status == 'sell':
+    #     positions = {p.symbol: p for p in api.list_positions()}
+    #     current_price = positions[sym].current_price
+    #     stop_price = float(current_price) * (1-off_set)
+    # elif status == 'buy':
+    #     current_price = pull_hist_data(api, sym, pd.Timestamp.now(), agg='minute', days='1 days')['close'][-1]
+    #     limit_price = float(current_price) * (1+off_set)
+    # else:
+    #     raise ValueError('Not a valid status. Must be buy or sell')
 
     if order_type == 'market':
         api.submit_order(
@@ -99,24 +99,24 @@ def make_order(api, status, sym, qty, order_type='market', off_set=0.005):
             time_in_force='day',
             )
 
-    elif order_type == 'stop':
-        api.submit_order(
-            symbol=sym,
-            qty=qty,
-            side=status,
-            type='stop',
-            time_in_force='day',
-            stop_price=stop_price
-            )
-
-    elif order_type == 'limit':
-        api.submit_order(
-            symbol=sym,
-            qty=qty,
-            side=status,
-            type='limit',
-            time_in_force='day',
-            limit_price=limit_price
-            )
+    # elif order_type == 'stop':
+    #     api.submit_order(
+    #         symbol=sym,
+    #         qty=qty,
+    #         side=status,
+    #         type='stop',
+    #         time_in_force='day',
+    #         stop_price=stop_price
+    #         )
+    #
+    # elif order_type == 'limit':
+    #     api.submit_order(
+    #         symbol=sym,
+    #         qty=qty,
+    #         side=status,
+    #         type='limit',
+    #         time_in_force='day',
+    #         limit_price=limit_price
+    #         )
     else:
         raise ValueError('Not a valid order_type. Must be market, stop, or limit')

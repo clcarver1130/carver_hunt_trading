74bedc0c43d6873e8808e9ed4cf2830681f0c01a
MarshallHunt <marshall.hunt@outlook.com>
2019-02-11 13:47:02 -0600

    Add files via upload

--- MarshallScripts/MarshallTesting.py
+++ MarshallScripts/MarshallTesting.py
@@ -105,7 +105,10 @@ def during_day_check(api, stock_list):
             stock = df.loc[df['Symbol'] == sym]
             max_price_loss = -.02
             print(stock)
-            if ((stock[1][10] - stock[1][11])/stock[1][10]) <= max_price_loss:
+            print(stock['Todays open'])
+            print(stock[0])
+            print(stock[1])
+            if ((stock[10] - stock[11])/stock[10]) <= max_price_loss:
                 stop_price = float(positions[sym].current_price) * .95
                 logging.info('Trying to sell {qty_to_sell} shares of {sym} stock for {price}'.format(qty_to_sell=positions[sym].qty, sym=sym,price=stop_price))
                 HelperFunctions.make_order(api, 'sell', sym, positions[sym].qty, order_type='stop', stop_price=stop_price)

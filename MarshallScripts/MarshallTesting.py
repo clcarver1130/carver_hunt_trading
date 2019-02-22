34eaabb0688915238bf23c4837509a64ec8192a5
MarshallHunt <marshall.hunt@outlook.com>
2019-02-22 12:53:07 -0600

    Add files via upload

--- MarshallScripts/MarshallTesting.py
+++ MarshallScripts/MarshallTesting.py
@@ -27,6 +27,7 @@ df['Buy'] = '0'
 df['Sell'] = '0'
 
 def main():
+    HelperFunctions.buy_with_excess_cash(api)
     logging.info('Starting Up...')
 
     schedule.every().day.at("09:31").do(first_of_day_trades, api, df)

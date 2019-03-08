7ce48c52ec5c386720030695a1d80cb50a96e5da
MarshallHunt <marshall.hunt@outlook.com>
2019-03-08 08:39:27 -0600

    Add files via upload

--- MarshallScripts/MarshallTesting.py
+++ MarshallScripts/MarshallTesting.py
@@ -84,7 +84,7 @@ def first_of_day_trades(api, dataframe):
             time.sleep(2)
 
         #wait to buy till markets have made initial day moves. This helps shield from large negative early day movements
-
+        stock_list.to_csv('testing.csv')
     else:
         df.iloc[0:0]
 
